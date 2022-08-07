b43f690aa8d209c0372f85d8acd20d8077f4d676
FalseProphet214 <cjcox2014@gmail.com>
2022-08-07 17:36:18 -0400

    Copied the dice rollers from the previous Repo

---
+++ Dice/AbilityScoreRoller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW5E_App.Dice
+{
+    internal class AbilityScoreRoller
+    {
+        public static AbilityScoreRoller Instance { get; } = new AbilityScoreRoller();
+        public Random d6 { get; } = new Random();
+        public AbilityScoreRoller()
+        { }
+
+        public int AbilityScore()
+        {
+            int rollSum = 0;
+            int lowest = 7;
+
+            for (int i = 0; i < 4; i++)
+            {
+                int roll = d6.Next(1, 7);
+                rollSum += roll;
+
+                if (roll < lowest)
+                    lowest = roll;
+            }
+
+            int result = rollSum - lowest;
+            return result;
+        }
+    }
+}
--- Player Things/Character Builder Forms/Character Builder.cs
+++ Player Things/Character Builder Forms/Character Builder.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
