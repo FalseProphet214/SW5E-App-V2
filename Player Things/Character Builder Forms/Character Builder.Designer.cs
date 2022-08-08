d1d14a50178c36c7ae0e9329eced006cec22c79d
FalseProphet214 <cjcox2014@gmail.com>
2022-08-07 20:11:09 -0400

    Added Species Description files for all cannon species from SW Began working on Character Creation Layout Added needed variables
    TO-DO
    - begin working on JSON for all species
    - Continue to work on character builder

--- Player Things/Character Builder Forms/Character Builder.Designer.cs
+++ Player Things/Character Builder Forms/Character Builder.Designer.cs
@@ -29,19 +29,60 @@ namespace SW5E_App.Player_Things.Character_Builder_Forms
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1201, 914);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1193, 886);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 72);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // Character_Builder
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1286, 813);
+            this.ClientSize = new System.Drawing.Size(1500, 938);
+            this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "Character_Builder";
             this.Text = "Character_Builder";
+            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
--- Player Things/Character Builder Forms/Character Builder.cs
+++ Player Things/Character Builder Forms/Character Builder.cs
@@ -12,9 +12,55 @@ namespace SW5E_App.Player_Things.Character_Builder_Forms
 {
     public partial class Character_Builder : Form
     {
+        #region VARIABLES
+
+        #region ABILITY SCORES
+
+        public int _str = 0;
+        public int _dex = 0;
+        public int _con = 0;
+        public int _int = 0;
+        public int _wis = 0;
+        public int _cha = 0;
+
+        public int increasePrimary = 0;
+        public int increaseSecondary = 0;
+
+        #endregion
+
+        #region SELECTED OBJECTS
+
+        public string _selectedSpecies;
+        public string _selectedClass;
+        public string _selectedBackground;
+
+        #endregion
+
+        #region FILE PATHS
+
+        string _speciesDescriptionsPath = @"Objects\Species\Descriptions";
+        string _speciesJsonPath = @"Objects\Species\JSON";
+        string _classesDescriptionsPath = @"Objects\Classes\Descriptions";
+        string _classesJsonPath = @"Objects\Classes\JSON";
+        string _backgroundJsonPath = @"Objects\Backgrounds\JSON";
+
+        #endregion
+
+        #region TOOLS
+
+        public int _skillsChecked;
+
+        #endregion
+
+        #endregion
+
+        #region FORM INITIALIZATION
+
         public Character_Builder()
         {
             InitializeComponent();
         }
+
+        #endregion
     }
 }
