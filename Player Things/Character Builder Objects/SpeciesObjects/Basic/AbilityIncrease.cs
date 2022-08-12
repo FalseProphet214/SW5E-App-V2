57a16a0cae139093dff1ec2eabfd96f7f6ef42fa
Corbin Cox <CJCox@Victoria.com>
2022-08-12 15:10:22 -0400

    started work on the JSON Files - Running into road block where some species which for you to pick one or the other abilities to add the increase too. Need to figure out how to code that into JSON

---
+++ Player Things/Character Builder Objects/SpeciesObjects/Basic/AbilityIncrease.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW5E_App.Player_Things.Character_Builder_Objects.SpeciesObjects.Basic
+{
+    public class AbilityIncrease
+    {
+        public Primary[] Primary { get; set; }
+        public Secondary[] Secondary { get; set; }
+     }
+}
---
+++ Player Things/Character Builder Objects/SpeciesObjects/Basic/Basic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW5E_App.Player_Things.Character_Builder_Objects.SpeciesObjects.Basic
+{
+    public class Basic
+    {
+        public Height Height { get; set; }
+        public Weight Weight { get; set; }
+        public AbilityIncrease AbilityIncrease { get; set; }
+        public string Size { get; set; }
+        public int Speed { get; set; }
+        public string[] Languages { get; set; }
+    }
+}
